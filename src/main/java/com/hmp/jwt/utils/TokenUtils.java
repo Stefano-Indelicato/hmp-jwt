d958ae3150c1a5cdba7ca5e87132cbde545e0a64
EE63756 <EE63756>
2020-06-30 16:11:44 +0200

    add audience and fix authorize

--- src/main/java/com/hmp/jwt/utils/TokenUtils.java
+++ src/main/java/com/hmp/jwt/utils/TokenUtils.java
@@ -30,7 +30,6 @@ public class TokenUtils {
 		claimsBuilder.subject(secret);
 		claimsBuilder.upn(id);
 		claimsBuilder.issuedAt(currentTimeInSecs);
-		claimsBuilder.audience("http://www.hmp.com");
 		claimsBuilder.expiresAt(currentTimeInSecs + duration);
 		claimsBuilder.groups(groups);
 
